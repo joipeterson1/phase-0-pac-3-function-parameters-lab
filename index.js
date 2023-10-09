03790657afd74c703bd05064815b0e60b64fa9a9
Dalecia <petersondalecia@gmail.com>
2023-10-09 19:59:04 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+function introductionWithLanguage() {
+    //define the functions
+}
+function introductionWithLanguage(Joi, Java) {
+    return (`Hi, my name is ${Joi} and I am learning to program in ${Java}.`) 
+}
+function introductionWithLanguageOptional() {
+    //define the functions
+}
+function introductionWithLanguageOptional(Joi, language = "JavaScript") {
+    return (`Hi, my name is ${Joi} and I am learning to program in ${language}.`) 
+}
